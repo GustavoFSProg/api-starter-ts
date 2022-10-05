c67f6d906326415f0d4364353e53cd0bad080b0e
Gustavo <gustavo.prog40@gmail.com>
2022-10-05 10:01:12 -0300

    users crud

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router, Request, Response } from 'express'
+import usersController from './controllers/usersController'
 
 const routes = Router()
 
 routes.get('/', function (req: Request, res: Response) {
   return res.send({ msg: ` 🌄 Api Running` })
 })
+
+
+routes.get('/all', usersController.getAll)
+routes.post('/register', usersController.registerUser)
+routes.put('/update/:id', usersController.updateUser)
+routes.delete('/delete/:id', usersController.deleteUser)
 
 export default routes
